75daacd05bbe7cfafb383a064478ede78ad0bdb8
Connor Holly <connorholly11@gmail.com>
2019-09-17 20:30:39 -0600

    adding form

--- friends/src/components/FriendsForm.js
+++ friends/src/components/FriendsForm.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const FriendsFrom = () => {
+const FriendsFrom = props => {
     const [friendform, setFriendForm] = useState({name: '', age: '', email: ''})
 
     const ChangeHandler = e => {
         setFriendForm({...friendform, [e.target.name]: e.target.value})
     }
 
-    
+    const submitForm = e => {
+        axiosWithAuth('/friends')
+            .then(response => {
+                console.log(response)
+            })
+            e.preventDefault();
+            const newFriend = {
+                ...friendform,
+                id: Date.now()
+            }
+        props.addNewFriend(newFriend);
+        setFriendForm({name: '', age: '', email: ''})
+    }
 
     return(
         <div>
